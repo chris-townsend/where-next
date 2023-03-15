3a18c0332e1bff905490b749e035547fc6087a23
chris-townsend <chris-townsend@live.co.uk>
2023-03-15 09:53:25 +0000

    Add Navbar.js to components folder & link to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Where Next</h1>
+      < Navbar />
     </div>
   );
 }
