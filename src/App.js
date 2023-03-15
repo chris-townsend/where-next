762a40b872563333c6179a2ee495b6783e01b437
chris-townsend <chris-townsend@live.co.uk>
2023-03-15 12:07:46 +0000

    Create App.module.css & add basic styling to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import styles from './App.module.css'
 import NavigationBar from './components/NavigationBar';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       < NavigationBar/>
     </div>
   );
