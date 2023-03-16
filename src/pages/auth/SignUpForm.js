15857052ec19766dd6895b5fb1f7774a138efe50
chris-townsend <chris-townsend@live.co.uk>
2023-03-16 12:10:49 +0000

    Add basic form to SignUpForm.js

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Image, Col, Row, Container } from "react-bootstrap";
+import { Image, Col, Row, Container, Form, Button } from "react-bootstrap";
 import appStyles from "../../App.module.css";
-
-
 
 const SignUpForm = () => {
   return (
@@ -11,6 +9,35 @@ const SignUpForm = () => {
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1>Sign Up</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control type="username" placeholder="Username" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password2"
+                placeholder="Confirm Password"
+                name="password2"
+              />
+              <Form.Text className="text-muted">
+                We'll never share your information with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link to="/signin">
@@ -18,10 +45,7 @@ const SignUpForm = () => {
           </Link>
         </Container>
       </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2`}
-      >
+      <Col md={6} className={`my-auto d-none d-md-block p-2`}>
         <Image
           className={`${appStyles.SignUpImage}`}
           src={
