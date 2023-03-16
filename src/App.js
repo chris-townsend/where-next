eef18eeda96ae27aabd982ad8aa6aa361683d529
chris-townsend <chris-townsend@live.co.uk>
2023-03-16 09:45:08 +0000

    Add routing to homepage from logo in NavigationBar.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import styles from './App.module.css'
-import NavigationBar from './components/NavigationBar';
-import { Container } from 'react-bootstrap';
-import { Route, Switch } from 'react-router-dom';
-
-
+import styles from "./App.module.css";
+import NavigationBar from "./components/NavigationBar";
+import { Container } from "react-bootstrap";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
-      < NavigationBar/>
+      <NavigationBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Homepage</h1>} />
@@ -17,7 +15,7 @@ function App() {
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
         </Switch>
         <Switch>
-          <Route exact path="/signup" render={() => <h1>Sign Up</h1> } />
+          <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
         </Switch>
       </Container>
     </div>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,27 +2,34 @@ import React from "react";
 import { Nav, Navbar, Form, FormControl, Container } from "react-bootstrap";
 import logo from "../assets/images/where-next-logo.png";
 import styles from "../styles/NavigationBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavigationBar = () => (
   <Navbar className={styles.NavigationBar} expand="lg" fixed="top">
     <Container>
-      <Navbar.Brand>
-        <img src={logo} alt="logo" height="65px" width="180px" />
-      </Navbar.Brand>
+      <NavLink to="/">
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="65px" width="180px" />
+        </Navbar.Brand>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Form className={styles.SearchForm}>
-          <FormControl
-            className={styles.SearchInput}
-            type="text"
-            placeholder="Search posts, profiles & more"
-          />
-          <button className={styles.SearchButton} aria-label="Search button" type="submit">
-            <i className="fas fa-search"></i>
-          </button>
-        </Form>
+      <Form className={styles.SearchForm}>
+        <FormControl
+          className={styles.SearchInput}
+          type="text"
+          placeholder="Search posts, profiles & more"
+        />
+        <button
+          className={styles.SearchButton}
+          aria-label="Search button"
+          type="submit"
+        >
+          <i className="fas fa-search"></i>
+        </button>
+      </Form>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-        <Nav.Item>
+          <Nav.Item>
             <Nav.Link>
               <i className="fas fa-home"></i> Home
             </Nav.Link>
