c99baf415c078b9ea39cdb941e5c39c7ad4bb432
chris-townsend <chris-townsend@live.co.uk>
2023-03-18 10:07:57 +0000

    Add basic sign-in template with image

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const SignInForm = () => {
+import {
+    Image,
+    Col,
+    Row,
+    Container,
+    Form,
+    Button,
+    Alert,
+  } from "react-bootstrap";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+
+function SignInForm() {
+
   return (
-    <div>SignInFormzzz</div>
-  )
+    <Row className={styles.Row}>
+      <Col className="my-auto p py-2-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>Sign In</h1>
+
+        </Container>
+        <Container className={`mt-2 text-center ${appStyles.Content}`}>
+          <Link className={styles.SignInLink} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.SignUpImage}`}
+          src={"https://res.cloudinary.com/diez1cpce/image/upload/v1679133248/login-image_vqabap.jpg"}
+        />
+      </Col>
+    </Row>
+  );
 }
 
-export default SignInForm
+export default SignInForm;
