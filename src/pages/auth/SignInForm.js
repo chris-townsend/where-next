784ff4ec9a7758944dabe7f254975f472d0026a4
chris-townsend <chris-townsend@live.co.uk>
2023-03-18 10:50:22 +0000

    Add form to SignInForm.js

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -2,28 +2,52 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import {
-    Image,
-    Col,
-    Row,
-    Container,
-    Form,
-    Button,
-    Alert,
-  } from "react-bootstrap";
+  Image,
+  Col,
+  Row,
+  Container,
+  Form,
+  Button,
+  Alert,
+} from "react-bootstrap";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
-
 function SignInForm() {
-
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p py-2-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Sign In</h1>
+          <Form>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+              />
+            </Form.Group>
 
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+              />
+            </Form.Group>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign in
+            </Button>
+          </Form>
         </Container>
         <Container className={`mt-2 text-center ${appStyles.Content}`}>
           <Link className={styles.SignInLink} to="/signup">
@@ -37,7 +61,9 @@ function SignInForm() {
       >
         <Image
           className={`${appStyles.SignUpImage}`}
-          src={"https://res.cloudinary.com/diez1cpce/image/upload/v1679133248/login-image_vqabap.jpg"}
+          src={
+            "https://res.cloudinary.com/diez1cpce/image/upload/v1679133248/login-image_vqabap.jpg"
+          }
         />
       </Col>
     </Row>
