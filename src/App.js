e1b7fcb4a1b007bedbcd61dca867baeba9693acf
chris-townsend <chris-townsend@live.co.uk>
2023-03-18 09:35:27 +0000

    Add SignInForm.js & link route to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults'
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Homepage</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm /> } />
           <Route exact path="/signup" render={() => <SignUpForm /> } />
           <Route render={() => <p> 404 Page not found</p>} />
         </Switch>
