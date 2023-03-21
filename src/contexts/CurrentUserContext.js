3d16113dcfdafed6721c5d12dab1f3edd9a3641f
chris-townsend <chris-townsend@live.co.uk>
2023-03-21 11:57:39 +0000

    Set axiosRes to refresh access token if 401 err & add axiosRes to handleMount

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
+import { response } from "msw";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -9,10 +11,11 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 
 export const CurrentUserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory()
 
   const handleMount = async () => {
     try {
-      const { data } = await axios.get("dj-rest-auth/user/");
+      const { data } = await axiosRes.get("dj-rest-auth/user/");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
@@ -22,6 +25,28 @@ export const CurrentUserProvider = ({ children }) => {
   useEffect(() => {
     handleMount();
   }, []);
+
+  useMemo(() => {
+    axiosRes.interceptors.response.use(
+      (response) => response,
+      async (err) => {
+        if (err.response?.status === 401){
+          try {
+            await axios.post('/dj-rest-auth/token/refresh/')
+          } catch(err){
+            setCurrentUser(prevCurrentUser => {
+              if (prevCurrentUser) {
+                history.push('/signin')
+              }
+              return null
+            })
+          }
+          return axios(err.config)
+        }
+        return Promise.reject(err)
+      }
+    )
+  })
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
