84342e01a90541c4903a8060aea1a014b2a9b32f
chris-townsend <chris-townsend@live.co.uk>
2023-03-24 11:58:32 +0000

    Add textFields to PostCreateForm with styling

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, handleChange } from "react";
 import { Form, Button, Row, Col, Container } from "react-bootstrap";
 
 import styles from "../../styles/PostCreateUpdate.module.css";
@@ -7,8 +7,50 @@ import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 import Upload from "../../assets/images/upload.png";
 
-
 function PostCreateForm() {
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+  const { title, content, image } = postData;
+
+  const textFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Green}`}
+        onClick={() => {}}
+      >
+        cancel
+      </Button>
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Green}`}
+        type="submit"
+      >
+        create
+      </Button>
+    </div>
+  );
+
   return (
     <Form>
       <Row>
@@ -23,21 +65,14 @@ function PostCreateForm() {
               >
                 <Asset src={Upload} message="Click to upload an image" />
               </Form.Label>
-              <div className="d-flex justify-content-center">
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Green}`}
-                  onClick={() => {}}
-                >
-                  cancel
-                </Button>
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Green}`}
-                  type="submit"
-                >
-                  create
-                </Button>
-              </div>
             </Form.Group>
+          </Container>
+        </Col>
+        <Col md={8} lg={3} className="d-none d-md-block p-0 p-md-2 ml-5">
+          <Container
+            className={`${appStyles.Content} ${styles.InputContainer}`}
+          >
+            {textFields}
           </Container>
         </Col>
       </Row>
