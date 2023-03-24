341cc816b574a247e0ff77bf142fe17e725238ab
chris-townsend <chris-townsend@live.co.uk>
2023-03-24 14:51:37 +0000

    Close #5 Add form data to API to allow users to create posts

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Form, Button, Row, Col, Container, Image } from "react-bootstrap";
 
 import styles from "../../styles/PostCreateUpdate.module.css";
@@ -7,13 +7,39 @@ import btnStyles from "../../styles/Button.module.css";
 import Asset from "../../components/Asset";
 import Upload from "../../assets/images/upload.png";
 
+import { axiosReq } from "../../api/axiosDefaults";
+import { useHistory } from "react-router";
+
 function PostCreateForm() {
+  const [errors, setErrors] = useState({});
   const [postData, setPostData] = useState({
     title: "",
     content: "",
     image: "",
   });
   const { title, content, image } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", imageInput.current.files[0]);
+
+    try {
+      const { data } = await axiosReq.post("/posts/", formData);
+      history.push(`/posts/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
 
   const handleChange = (event) => {
     setPostData({
@@ -69,7 +95,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -104,6 +130,7 @@ function PostCreateForm() {
                 accept="image/*"
                 className="d-none"
                 onChange={handleChangeImage}
+                ref={imageInput}
               />
             </Form.Group>
           </Container>
