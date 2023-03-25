70f350a17fa195b11ada98f0996a13ab062072ca
chris-townsend <chris-townsend@live.co.uk>
2023-03-25 10:20:52 +0000

    Fix #32 Add missing parenthesis to useParams variable in PostPage function

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostCreateUpdate.module.css";
 
 function PostPage() {
+  const { id } = useParams();
+  const [post, setPost] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
+        setPost({ results: [post] });
+        console.log(post);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
+
   return (
     <Row className={`${styles.RowWidth}`}>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
