7717b57d44073ec6f4900d7d171c4841535b27f8
chris-townsend <chris-townsend@live.co.uk>
2023-03-27 08:47:58 +0000

    Add list of posts to home/feed & add logic to handle filtered requests to the api

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,20 +1,62 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Col, Row, Container } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import { axiosReq } from "../../api/axiosDefaults";
 
+
+import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
+import NoResults from "../../assets/images/no-results.png";
 
-function PostsPage() {
+function PostsPage({ message, filter = "" }) {
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathName } = useLocation();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchPosts();
+  }, [filter, pathName]);
+
   return (
     <Row className={`${styles.RowWidth}`}>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
       </Col>
     </Row>
   );
-};
+}
 
 export default PostsPage;
