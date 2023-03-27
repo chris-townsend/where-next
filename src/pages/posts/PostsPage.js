9f7c3e5c5da16b9f5dedaa9372ab68b246b6c178
chris-townsend <chris-townsend@live.co.uk>
2023-03-27 12:31:43 +0000

    Close #26 Add infinite scroll functionality for posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -9,6 +9,7 @@ import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import NoResults from "../../assets/images/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -57,18 +58,15 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              <InfiniteScroll 
-              children={
-               posts.results.map((post) => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              )) 
-              }
-              dataLength={posts.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!posts.next}
-              next={() => {}}
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
               />
-              
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
