c89ff4098af520640a92ef3d79b05646ae79642c
chris-townsend <chris-townsend@live.co.uk>
2023-03-29 07:39:05 +0000

    Close #27 Add updated_date to Comment.js & add styling

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { Media } from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 
+import styles from "../../styles/Comment.module.css";
+
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, comment } = props;
+  const { profile_id, profile_image, owner, comment, updated_date } = props;
 
   return (
     <div>
@@ -15,6 +17,7 @@ const Comment = (props) => {
         </Link>
         <Media.Body className="align-self-center ml-2">
           <span>{owner}</span>
+          <span className={styles.Date}>{updated_date}</span>
           <p>{comment}</p>
         </Media.Body>
       </Media>
