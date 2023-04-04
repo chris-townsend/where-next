607929a0bf5f012b01f3d239a8c871544d491592
chris-townsend <chris-townsend@live.co.uk>
2023-04-04 12:53:28 +0000

    Add handleSubmit & handleChange to ContactCreateForm.js

--- src/pages/contact/ContactCreateForm.js
+++ src/pages/contact/ContactCreateForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form, Button, Container, Alert } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
@@ -8,7 +9,40 @@ import styles from "../../styles/ContactCreateForm.module.css";
 
 const ContactCreateForm = () => {
   const [errors, setErrors] = useState({});
+
+  const [contactData, setContactData] = useState({
+    subject: "",
+    message: "",
+  });
+
+  const { subject, message } = contactData;
+
   const history = useHistory();
+
+  const handleChange = (event) => {
+    setContactData({
+      ...contactData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("subject", subject);
+    formData.append("message", message);
+
+    try {
+      await axiosReq.post("/contact/", formData);
+      history.push("/");
+    } catch (err) {
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+
   const textFields = (
     <div className="text-center pt-0 pt-lg-4">
       <Form.Group>
@@ -17,24 +51,24 @@ const ContactCreateForm = () => {
           className={appStyles.Input}
           type="text"
           name="subject"
-          value={() => {}}
-          onChange={() => {}}
+          value={subject}
+          onChange={handleChange}
         />
       </Form.Group>
-      {errors?.title?.map((message, idx) => (
-        <Alert variant="danger" key={idx}>
+      {errors?.subject?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
           {message}
         </Alert>
       ))}
       <Form.Group>
         <Form.Label className={appStyles.TextColor}>Message</Form.Label>
         <Form.Control
+          className={appStyles.Input}
           as="textarea"
           rows={6}
           name="message"
-          className={appStyles.Input}
-          value={() => {}}
-          onChange={() => {}}
+          value={message}
+          onChange={handleChange}
         />
       </Form.Group>
       {errors?.message?.map((message, idx) => (
@@ -58,7 +92,7 @@ const ContactCreateForm = () => {
   );
 
   return (
-    <Form className="mt-1 mt-md-4" onSubmit={() => {}}>
+    <Form className="mt-1 mt-md-4" onSubmit={handleSubmit}>
       <Container
         className={`${styles.Container} d-flex flex-column justify-content-center pb-3 pt-2`}
       >
