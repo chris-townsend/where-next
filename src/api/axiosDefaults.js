170a642e4d3555aa4d25456d8e96c3cc6207a9b9
chris-townsend <chris-townsend@live.co.uk>
2023-04-16 12:03:56 +0000

    Format & add comments to axiosDefaults.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,10 +1,15 @@
+// API
 import axios from "axios";
 
+// Set the default configuration for axios requests
+axios.defaults.baseURL = "https://where-next-drf-api.herokuapp.com/"; // Base URL for API requests
+// Default content type for POST requests
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+// Send cookies with cross-origin requests
+axios.defaults.withCredentials = true;
 
-axios.defaults.baseURL = 'https://where-next-drf-api.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+// Create an instance of axios for request interceptors
+export const axiosReq = axios.create();
 
-export const  axiosReq  = axios.create();
-
+// Create an instance of axios for response interceptors
 export const axiosRes = axios.create();
